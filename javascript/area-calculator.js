8402e4d87658b7cd9fe64b4ef3e09add3e18278d
mahisurrahman <zordanblog009@gmail.com>
2023-08-19 08:17:13 +0600

    Added 3rd Video Works

--- javascript/area-calculator.js
+++ javascript/area-calculator.js
@@ -8,4 +8,26 @@ document.getElementById('triangleButton').addEventListener('click', function(){
     const heightValueString = heightField.value;
     const height = parseFloat(heightValueString);
 
+    const area = 0.5*base*height;
+
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
 })
+
+
+function calculateRectangleArea(){
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+
+    const area = length*width;
+    
+    console.log(area);
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+}
