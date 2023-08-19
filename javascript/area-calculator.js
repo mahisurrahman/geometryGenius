fb2f5490adb77acf96ea3c555d1e7f529ee3f948
mahisurrahman <zordanblog009@gmail.com>
2023-08-20 02:07:21 +0600

    Added the Ellipse Section

--- javascript/area-calculator.js
+++ javascript/area-calculator.js
@@ -1,5 +1,5 @@
 
-document.getElementById('triangleButton').addEventListener('click', function(){
+document.getElementById('triangleButton').addEventListener('click', function () {
     const baseField = document.getElementById('triangle-base');
     const baseValueString = baseField.value;
     const base = parseFloat(baseValueString);
@@ -8,14 +8,14 @@ document.getElementById('triangleButton').addEventListener('click', function(){
     const heightValueString = heightField.value;
     const height = parseFloat(heightValueString);
 
-    const area = 0.5*base*height;
+    const area = 0.5 * base * height;
 
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
 })
 
 
-function calculateRectangleArea(){
+function calculateRectangleArea() {
     const widthField = document.getElementById('rectangle-width');
     const widthValueText = widthField.value;
     const width = parseFloat(widthValueText);
@@ -24,10 +24,38 @@ function calculateRectangleArea(){
     const lengthValueText = lengthField.value;
     const length = parseFloat(lengthValueText);
 
-    const area = length*width;
-    
+    const area = length * width;
+
     console.log(area);
 
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = area;
+}
+
+function calculateParalellogramArea() {
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('paralellogram-height');
+    const area = base * height;
+    setElementInnerText('paralellogram-area', area);
+
+}
+
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
+
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('majorRadius');
+    const minorRadius = getInputValue('minorRadius');
+    const area = 3.14 * majorRadius * minorRadius;
+
+    setElementInnerText('ellipse-area', area);
 }
